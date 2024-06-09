c283c05c3c03f19d22a9fffa403f88bef9dac50f
Bulldo344 <policelaw14@gmail.com>
2024-06-09 16:59:57 -0400

    removed debug statement

--- rblxopencloud/__init__.py
+++ rblxopencloud/__init__.py
@@ -26,7 +26,6 @@ import requests
 
 VERSION: str = "2.0.0"
 VERSION_INFO: Literal['alpha', 'beta', 'final'] = "beta"
-print(VERSION_INFO)
 
 user_agent: str = f"rblx-open-cloud/{VERSION} \
 (https://github.com/treeben77/rblx-open-cloud)"
